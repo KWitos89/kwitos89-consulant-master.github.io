ecf8ab30e46a12ed76b6e8b921f78038b7a219b5
Krzysztog Witos <kwitos89@gmail.com>
2021-02-17 21:36:50 +0100

    Add

--- js/menuMobile.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector('.menu_hamburger');
-const menu = document.querySelector('.menu');
-
-const handleClick = () => {
-  hamburger.classList.toggle('menu_hamburger:active');
-  menu.classList.toggle('menu:active');
-}
-
-hamburger.addEventListener('click' ,handleClick);
