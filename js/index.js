91269d2c3156b56da0c38784628435e770289061
Krzysztog Witos <kwitos89@gmail.com>
2021-02-19 22:51:27 +0100

    ..ss

--- js/index.js
+++ js/index.js
@@ -4,10 +4,12 @@ const linkNav = document.querySelector('.link_nav');
 
 
 
+
 hamburger.addEventListener("click", () => {
     linkNav.classList.toggle("open");
-    hamburger.classList.toggle("menu_hamburger-active");
-    
+    hamburger.classList.toggle("close");
+
 });
+
 
 
